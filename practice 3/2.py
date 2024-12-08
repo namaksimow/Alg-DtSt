38def4e3d611360e392ae3fd241867313920a852
namaksimow <144584830+namaksimow@users.noreply.github.com>
2024-12-08 16:47:55 +0400

    5 commit. rename folders, add 1 exersice for 4 practice

--- practice 3/2.py
+++ practice 3/2.py
@@ -3,8 +3,8 @@ import random
 
 def create_matrix(count_project, max_sum_investment):
     min_sum = 0
-    low_border = 0
-    high_border = 9
+    low_border = 2
+    high_border = 8
     count_row = max_sum_investment // 10 + 1
     count_column = count_project + 1
     matrix = [[0 for _ in range(count_column)] for _ in range(count_row)]
@@ -19,10 +19,10 @@ def create_matrix(count_project, max_sum_investment):
             matrix[i][j] = random.randint(low_border, high_border)
         low_border += 10
         high_border += 10
-
-    for i in range(count_row):
-        print(matrix[i])
-    print("")
+    print("\ntable of start matrix")
+    for row in matrix:
+        print(" | ".join(f"{num:3d}" for num in row))
+    print("\ntable of max profit")
     return matrix, profit_matrix
 
 
@@ -47,8 +47,8 @@ def fill_profit_matrix(matrix, profit_matrix, count_project, max_sum_investment)
 
                 profit_matrix[j][i] = max(max_variants)
 
-    for i in range(count_row):
-        print(profit_matrix[i])
+    for row in profit_matrix:
+        print(" | ".join(f"{num:3d}" for num in row))
 
     print(profit_matrix[-1][-1])
 
@@ -62,5 +62,3 @@ def solve():
 
 if __name__ == "__main__":
     solve()
-
-
---
+++ practice 4/1.py
@@ -0,0 +1,73 @@
+import random
+import string
+
+
+def grouping_by_time(tasks: dict) -> (dict, dict):
+    first_group = {}
+    second_group = {}
+    for key in tasks:
+        time_tuple = tasks[key]
+        if time_tuple[0] <= time_tuple[1]:
+            first_group[key] = tasks[key]
+        else:
+            second_group[key] = tasks[key]
+    return first_group, second_group
+
+
+def input_task_time(count_task: int) -> dict:
+    alphabet = list(string.ascii_lowercase)
+    tasks: dict = {}
+    for i in range(count_task):
+        tasks[alphabet[i].upper()] = (i + 1, random.randint(1, count_task))
+
+    return tasks
+
+
+def sort_group(first_group, second_group):
+    first_group = dict(sorted(first_group.items(), key=lambda pair: pair[1][0]))
+    second_group = dict(sorted(second_group.items(), key=lambda pair: pair[1][0], reverse=True))
+    general_group = {**first_group, ** second_group}
+    return general_group
+
+
+def start_time(general_group):
+    time_end_first = 0
+    key = next(iter(general_group))
+    value = general_group[key][0]
+    time_end_second = value
+    return time_end_first, time_end_second
+
+
+def task_distribution(general_group):
+    time_end_first, time_end_second = start_time(general_group)
+    
+    first_employee = {}
+    second_employee = {}
+    for key in general_group:
+        pair_tuple = general_group[key]
+        first_employee[key] = (time_end_first, time_end_first + pair_tuple[0])
+        if time_end_first + pair_tuple[0] <= time_end_second:
+            second_employee[key] = (time_end_second, time_end_second + pair_tuple[1])
+        else:
+            second_employee[key] = (time_end_first + pair_tuple[0], time_end_first + pair_tuple[0] + pair_tuple[1])
+        time_end_first += pair_tuple[0]
+        time_end_second += pair_tuple[1]
+
+    return general_group, first_employee, second_employee
+
+
+def print_answer(general_group, first_employee, second_employee):
+    print(f"{general_group} - all tasks\n\n{first_employee} - first employee\n\n{second_employee} - second employee")
+
+
+def solve():
+    n = int(input("Input count tasks "))
+    task_dict = input_task_time(n)
+    first_group, second_group = grouping_by_time(task_dict)
+    general_group = sort_group(first_group, second_group)
+    general_group, first_employee, second_employee = task_distribution(general_group)
+    print_answer(general_group, first_employee, second_employee)
+
+
+if __name__ == "__main__":
+    solve()
